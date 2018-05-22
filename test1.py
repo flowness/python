e5b065dfc234900659462f39eeba605e746de8c5
flowness <yocohen@gmail.com>
2018-05-22 12:14:43 +0300

    first

---
+++ test1.py
@@ -0,0 +1,17 @@
+from __future__ import print_function
+from pyfcm import FCMNotification
+
+# push_service = FCMNotification(api_key="AAAAktMOnHY:APA91bHzPsOnUuFU26Jjdl3NIhb9Wp540eTvWSNhBxcNS6zRSrPSzNDZqtOYwJqy0QcK7AGwEgn4FkEp5S_8As9dXLIDjc-VfU6sR1DdC4uEELfk1dPKwBzYchdi9Wm4JnGPQ72ayUFP")
+
+# registration_id = "cvqYcEhpd2Y:APA91bFuz5IKfQ0vVobGw_aFnP981MrthWSIVuZEqtl6CkOsnOwiYbAqnxXV-q61y1ogDEOeo5-n7Thj0mR36MJeudpR_2FuGQWKlJssWP0WgiDsK1zSx_7JEs1rjsolfGZWmSP009Uj"
+# message_title = "Uber update"
+# message_body = "Hi john, your customized news for today is ready"
+# result = push_service.notify_single_device(registration_id=registration_id, message_title=message_title, message_body=message_body)
+
+# # Send to multiple devices by passing a list of ids.
+# registration_ids = [registration_id]
+# message_title = "Uber update"
+# message_body = "Hope you're having fun this weekend, don't forget to check today's news"
+# result = push_service.notify_multiple_devices(registration_ids=registration_ids, message_title=message_title, message_body=message_body)
+
+print("ddd")
